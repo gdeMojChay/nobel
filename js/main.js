3bba3b62cd2e447510c553573dfd89db7253e123
gdeMojChay <thetypgame@gmail.com>
2021-01-25 14:16:08 +0400

    change buuton, add name in bio info and load bio

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $( document ).ready(function(){
     $('.readmore').click(function() {
         $('.bio').show();
+        $('#name').text($(this).parent().children('.name').children('h3').text())
+        // alert($(this).attr('id'))
+        $('.bio').children('span').load('./bio.html .'+$(this).attr('id'))
     });
     $('.close').click(function(){
         $(this).parent().parent().hide()
