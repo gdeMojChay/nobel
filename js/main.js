50cf9fd63344ad1db6ec8bca26ebffa98492497c
gdeMojChay <thetypgame@gmail.com>
2021-01-25 14:29:47 +0400

    подчистил код

--- js/main.js
+++ js/main.js
@@ -2,9 +2,7 @@ $( document ).ready(function(){
     $('.readmore').click(function() {
         $('.bio').show();
         $('#name').text($(this).parent().children('.name').children('h3').text());
-        // alert('./bio.html ".' + $(this).attr('id') + '"')
         $('.bio').children('span').load('./bio.html #'+$(this).attr('id'));
-        alert('bio!')
     });
     $('.close').click(function(){
         $(this).parent().parent().hide()
