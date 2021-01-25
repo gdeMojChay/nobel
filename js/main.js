08d9ee512136cf6085cb59fa166b403a8ee8e4a4
gdeMojChay <thetypgame@gmail.com>
2021-01-25 14:23:04 +0400

    -/-

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $( document ).ready(function(){
     $('.readmore').click(function() {
         $('.bio').show();
-        $('#name').text($(this).parent().children('.name').children('h3').text())
+        $('#name').text($(this).parent().children('.name').children('h3').text());
         // alert('./bio.html ".' + $(this).attr('id') + '"')
-        $('.bio').children('span').load('./bio.html #'+$(this).attr('id'))
+        $('.bio').children('span').load('./bio.html #'+$(this).attr('id'));
     });
     $('.close').click(function(){
         $(this).parent().parent().hide()
